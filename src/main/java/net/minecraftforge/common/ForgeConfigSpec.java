f85be659689afea8834062c252399afd08340b1a
Paint_Ninja <PaintNinja@users.noreply.github.com>
2024-10-26 16:30:27 +0100

    Promote "constantly correcting config" warning to error

--- src/main/java/net/minecraftforge/common/ForgeConfigSpec.java
+++ src/main/java/net/minecraftforge/common/ForgeConfigSpec.java
@@ -707,15 +707,8 @@ public class ForgeConfigSpec extends UnmodifiableConfigWrapper<UnmodifiableConfi
         public boolean hasComment() { return !this.comment.isEmpty(); }
         public String buildComment() { return buildComment(List.of("unknown", "unknown")); }
         public String buildComment(final List<String> path) {
-            if (comment.stream().allMatch(String::isBlank)) {
-                if (FMLEnvironment.production)
-                    LOGGER.warn(Logging.CORE, "Detected a comment that is all whitespace for config option {}, which causes obscure bugs in Forge's config system and will cause a crash in the future. Please report this to the mod author.",
-                            DOT_JOINER.join(path));
-                else
-                    throw new IllegalStateException("Can not build comment for config option " + DOT_JOINER.join(path) + " as it comprises entirely of blank lines/whitespace. This is not allowed as it causes a \"constantly correcting config\" bug with NightConfig in Forge's config system.");
-
-                return "A developer of this mod has defined this config option with a blank comment, which causes obscure bugs in Forge's config system and will cause a crash in the future. Please report this to the mod author.";
-            }
+            if (comment.stream().allMatch(String::isBlank))
+                throw new IllegalStateException("Can not build comment for config option " + DOT_JOINER.join(path) + " as it comprises entirely of blank lines/whitespace. This is not allowed as it causes a \"constantly correcting config\" bug with NightConfig in Forge's config system.");
 
             return LINE_JOINER.join(comment);
         }
